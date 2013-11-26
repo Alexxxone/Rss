a5039ad1d4fa4f6d0a56fac4289fcf294119e32c
Nataly <natalyasvd@gmail.com>
2013-11-26 14:02:03 +0200

    Paginate and image

---
+++ config/initializers/feedzirra.rb
@@ -0,0 +1,2 @@
+Feedzirra::Feed.add_common_feed_entry_element('media:thumbnail', :value => :url, :as => :image)
+Feedzirra::Feed.add_common_feed_entry_element("enclosure", :as => :image, :value => :url, :with => {:type => "image/jpg"})
